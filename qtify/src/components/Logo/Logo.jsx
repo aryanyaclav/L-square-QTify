66e3fa22e8f615754b10b2ad5787d0db5e27c6f7
aryan <aycool19@gmail.com>
2024-03-12 23:17:45 +0530

    test cases fixes

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import logo from "../../assests/qtify-logo.png"
+import styles from "./Logo.module.css"
 
 export default function Logo() {
   return (
     <>
-        <img src ={logo} alt="Qtify"></img>
+        <img className={styles.logo} src ={logo} alt="Qtify"></img>
     </>
   )
 }
