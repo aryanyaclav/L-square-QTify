73ae8810f707d484e1d989dde449eba01f71a3c7
aryan <aycool19@gmail.com>
2024-03-12 22:49:26 +0530

    navbar improved

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from "../../assests/qtify-logo.png"
+
+export default function Logo() {
+  return (
+    <>
+        <img src ={logo} alt="Qtify"></img>
+    </>
+  )
+}
--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Styles from "./Navbar.module.css"
 import SearchBar from "../SearchBar/SearchBar.jsx"
 import FeedbackButton from "../FeedbackButton/FeedbackButton.jsx";
+import Logo from "../Logo/Logo.jsx";
 
 function Navbar(){
     return (
         <>
             <nav className={Styles.navbar}>
-                <h2>Qtify</h2>
+                <Logo />
                 <SearchBar />
                 <FeedbackButton />
             </nav>
